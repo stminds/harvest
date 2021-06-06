98776f84fba986340e8f1dce282265a00525ed6f
jiajun.guo <jiajun.guo@163.com>
2021-06-06 17:45:40 +0800

    init

---
+++ toolkits/src/main/java/cn/inmer/toolkits/ToolkitsApplication.java
@@ -0,0 +1,13 @@
+package cn.inmer.toolkits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolkitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolkitsApplication.class, args);
+    }
+
+}
---
+++ toolkits/src/test/java/cn/inmer/toolkits/ToolkitsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.inmer.toolkits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolkitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
