8877cfce9efd95315fcf008047cfa6ac080b50fe
jiajun.guo <jiajun.guo@iyouke.com>
2022-07-14 22:31:23 +0800

    Java8 流

--- harvest-experiment/src/main/java/com/stminds/experiment/stream/CreateStream.java
+++ harvest-experiment/src/main/java/com/stminds/experiment/stream/CreateStream.java
@@ -1,5 +1,6 @@
 package com.stminds.experiment.stream;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CreateStream {
@@ -8,6 +9,7 @@ public class CreateStream {
         Stream<Integer> integerStream = Stream.of(1).filter(integer -> false);
         Stream<String> generate = Stream.generate(() -> "1");
         Stream<Integer> iterate = Stream.iterate(0, a -> a + 1);
+        Stream.generate(() -> "1").collect(Collectors.toMap(key -> key, v -> v, (r1, r2) -> r1));
     }
 
 }
